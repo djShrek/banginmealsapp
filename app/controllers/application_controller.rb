c30a86f998dd266e5a252711db780966ea11f1f8
djShrek <boho527@gmail.com>
2016-11-10 21:46:00 -0800

    update scopes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery except: :get_meals
 
   def get_meals
-    @meals = Meal.all
+    @clean_meals = Meal.clean_meals
+    @specialty_meals = Meal.specialty_meals
+    @meals = { clean_meals: @clean_meals, specialty_meals: @specialty_meals }
     respond_to do |format|
       format.js { render json: @meals, callback: params['callback'] }
       format.json { render json: @meals }
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,5 +1,8 @@
 class Meal < ApplicationRecord
+  scope :active_meals, -> { where(status: "active") }
+  scope :clean_meals, -> { active_meals.where(type: "CleanMeal") }
+  scope :specialty_meals, -> { active_meals.where(type: "SpecialtyMeal") }
+  
   validates :name, presence: true
   validates :description, presence: true
-
 end
