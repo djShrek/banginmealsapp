fd4a491443ca40bec1015ec466fcb7669ea57ca9
djShrek <boho527@gmail.com>
2017-01-31 22:35:27 -0800

    fix cleanmeal typo

--- spec/controllers/meals_controller_spec.rb
+++ spec/controllers/meals_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe MealsController, type: :controller do
-
+  context "#create" do
+    xit "returns 201"
+  end
 end
--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Meal, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  context "validations" do
+    it "validates stuff"
+  end
 end
