967a5cdb5c20d593c7ed237e3fca700a8bee6f50
djShrek <boho527@gmail.com>
2017-02-01 22:57:25 -0800

    add admin class

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :meals
+  devise_for :admins
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   get "get_meals", to: "application#get_meals"
 
   root to: "admin#home"
+
+  resources :meals
 
 end
